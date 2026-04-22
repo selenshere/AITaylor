60e5a0d245e24e78d8ca326425e8872492f120c0
Selen <110853498+selenshere@users.noreply.github.com>
2026-04-22 22:51:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -734,14 +734,3 @@ submitBtn?.addEventListener("click", () => {
   });
 });
 }
-
-const saved = localStorage.getItem("taylor_task_state");
-
-if (saved) {
-  try {
-    Object.assign(state, JSON.parse(saved));
-  } catch (e) {
-    console.error("State load error:", e);
-    localStorage.removeItem("taylor_task_state");
-  }
-}
